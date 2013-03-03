ec330e835ca1103a309707beac902107ed7c5296
Ramon Niet <Ramon.Niet@tu-dortmund.de>
2013-03-03 23:20:21 +0100

    added slightly more complex example, however still open question on class / instance variable handling

--- Classes/example.py
+++ Classes/example.py
@@ -1,7 +1,21 @@
 #This is a new-type class, inheriting from object
 class Animal(object):
-	def __init__(self, name):
+	"""Makes cute animals."""
+	is_alive = True
+	health = "good"
+	def __init__(self, name, age):
 		self.name = name
+		self.age = age
+	def description(self):
+		print self.name
+		print self.age
 		
-zebra = Animal("Jeffrey")
-print zebra.name
+hippo = Animal("Jack", 24)
+hippo.description()
+
+sloth = Animal("Dieter", 35)
+ocelot = Animal("Klaus", 93)
+
+print hippo.health
+print sloth.health
+print ocelot.health
